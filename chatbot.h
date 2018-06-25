dfe1f0097328c82a71ab84d97bb0516b35c8868f
Mahmoud Ahmed <mahmoudaahmedd@gmail.com>
2018-06-25 04:11:45 +0200

    Add files via upload

--- chatbot.h
+++ chatbot.h
@@ -1,15 +1,15 @@
-/** 
+/**
  *  @file    chatbot.h
  *  @author  Mahmoud Ahmed Tawfik (mahmoudahmedd)
- *  @date    21/05/2018 
- *  @version 1.1 
+ *  @date    21/05/2018
+ *  @version 1.1
  *
  *  @brief Chatbot using a knowledge in database
  *
  *  @section DESCRIPTION
  *
- *  This is a little program that reads list of data from 
- *	a specified file and process the query and use the knowledge 
+ *  This is a little program that reads list of data from
+ *	a specified file and process the query and use the knowledge
  *  in database then return response.
  *
  */
@@ -29,19 +29,19 @@ class Chatbot
 {
 private:
     string chatbotName;
-	string databasePath;
-	vector<string> query;
+    string databasePath;
+    vector<string> query;
     vector<string> vResponses[3];
-	bool status;
-	static string punctuation;
-	void loadData();
-	void queryProcessing(string &_query);
-	bool isPunctuation(char c);
+    bool status;
+    static string punctuation;
+    void loadData();
+    void queryProcessing(string &_query);
+    bool isPunctuation(char c);
 public:
     Chatbot();
     Chatbot(string _chatbotName,string _databasePath);
-	string getResponse(string _query);
-	bool getStatus();
+    string getResponse(string _query);
+    bool getStatus();
 };
 
 #endif //CHATBOT_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
-/** 
+/**
  *  @file    main.cpp
  *  @author  Mahmoud Ahmed Tawfik (mahmoudahmedd)
- *  @date    21/05/2018 
- *  @version 1.1 
+ *  @date    21/05/2018
+ *  @version 1.1
  *
  *  @brief Chatbot using a knowledge in database
  *
  *  @section DESCRIPTION
  *
- *  This is a little program that reads list of data from 
- *	a specified file and process the query and use the knowledge 
+ *  This is a little program that reads list of data from
+ *	a specified file and process the query and use the knowledge
  *  in database then return response.
  *
  */
@@ -23,13 +23,13 @@ const string APP_NAME = "Lena";
 
 int main()
 {
-	Chatbot chatbot(APP_NAME,"database.txt");
-	string query;
-	while(chatbot.getStatus())
-	{
-		cout << "> ";
+    Chatbot chatbot(APP_NAME,"database.txt");
+    string query;
+    while(chatbot.getStatus())
+    {
+        cout << "> ";
         getline(cin, query);
         cout << chatbot.getResponse(query) << endl;
-	}
+    }
     return 0;
 }
